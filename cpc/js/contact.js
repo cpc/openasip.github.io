467eaf8aa4a51630d7dce6f72a983421c1123ab7
Joonas Multanen <joonas.multanen@tuni.fi>
2025-10-14 12:58:41 +0300

    Remove CPC pages

--- cpc/js/contact.js
+++
@@ -1,6 +0,0 @@
-var foo = window.location.href;
-var bar = foo.split("/");
-var asd = "if.inut.stsil@";
-var qwe = 2;
-var linktext = bar[bar.length - qwe] + asd.split('').reverse().join('') ;
-document.write("<a href='" + "mail" + "to:" + "cpc-group" + asd.split('').reverse().join('') + "'>" + "cpc-group" + "</a>");
